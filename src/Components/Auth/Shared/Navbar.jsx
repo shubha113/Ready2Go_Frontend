898a28b5ac0360488f261fb79c9878be21223c7f
unknown <sk9779047843@gmail.com>
2024-11-17 00:51:56 +0530

    Navbar complete for the small device and big device also

--- src/Components/Auth/Shared/Navbar.jsx
+++ src/Components/Auth/Shared/Navbar.jsx
@@ -1,18 +1,22 @@
-import React, { useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import './Navbar.css';
-import { logout } from '../../../Redux/actions/userAction';
-import { toast } from 'react-toastify';
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import "./Navbar.css";
+import { logout } from "../../../Redux/actions/userAction";
+import { toast } from "react-toastify";
+import Logo from "../../../Assets/logo.png";
 
 const Navbar = () => {
+  const [isMenuActive, setMenuActive] = useState(false);
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { isAuthenticated, user, message, error } = useSelector(state => state.user);
+  const { isAuthenticated, user, message, error } = useSelector(
+    (state) => state.user
+  );
 
   useEffect(() => {
     if (message) {
-      navigate('/login')
+      navigate("/login");
       toast.success(message);
       dispatch({ type: "clearMessage" });
     }
@@ -26,35 +30,72 @@ const Navbar = () => {
     dispatch(logout());
   };
 
+  const toggleMenu = () => {
+    setMenuActive(!isMenuActive);
+  };
+
   return (
     <header className="header">
       <nav>
-        <ul className="nav-links">
-          <li><Link to="/">Home</Link></li>
-          
-          {user?.role === 'user' && (
+        <div className="logo">
+          <Link to="/">
+            <img src={Logo} alt="Ready2Go" />
+          </Link>
+        </div>
+        <ul className={`nav-links ${isMenuActive ? "active" : ""}`}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+
+          {user?.role === "user" && (
             <>
-              <li><Link to="/create-order">Create Order</Link></li>
-              <li><Link to="/track-order">Track Order</Link></li>
+              <li>
+                <Link to="/create-order">Create Order</Link>
+              </li>
+              <li>
+                <Link to="/track-order">Track Order</Link>
+              </li>
             </>
           )}
-          {(user?.role === 'driver' || user?.role === 'company') && (
-            <li><Link to="/take-delivery">Take Delivery</Link></li>
+          {(user?.role === "driver" || user?.role === "company") && (
+            <li>
+              <Link to="/take-delivery">Take Delivery</Link>
+            </li>
           )}
 
           {isAuthenticated ? (
             <>
-              <li><Link to="/profile">Profile</Link></li>
-              <li><Link to="/history">History</Link></li>
-              <li onClick={handleLogout}><Link>Logout</Link></li>
+              <li>
+                <Link to="/profile">Profile</Link>
+              </li>
+              <li>
+                <Link to="/history">History</Link>
+              </li>
+              <li onClick={handleLogout}>
+                <div className="button-logout">
+                  <Link className="button-link">Logout</Link>
+                </div>
+              </li>
             </>
           ) : (
             <>
-              <li><Link to="/register">Register</Link></li>
-              <li><Link to="/login">Login</Link></li>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
             </>
           )}
         </ul>
+        <div
+          className={`hamburger ${isMenuActive ? "hamburger-active" : ""}`}
+          onClick={toggleMenu}
+        >
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
       </nav>
     </header>
   );
