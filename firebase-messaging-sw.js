6da6946225b1ed32db07bc227eb7a572778d38dd
shubha113 <shubhamkumarrai212@gmail.com>
2024-12-06 11:10:22 +0530

    firebase

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,31 +1,31 @@
-// Explicitly specify full URLs for imports
+
 importScripts('https://www.gstatic.com/firebasejs/11.0.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/11.0.1/firebase-messaging-compat.js');
 
-// Ensure the script is a valid JavaScript file
-(function() {
-    // Initialize Firebase
-    firebase.initializeApp({
-        apiKey: "AIzaSyBTMGjYkoraNTcr_MajS83QV6EjR3on8KY",
-        authDomain: "readytogo-e7bde.firebaseapp.com",
-        projectId: "readytogo-e7bde",
-        storageBucket: "readytogo-e7bde.firebasestorage.app",
-        messagingSenderId: "960914370023",
-        appId: "1:960914370023:web:0f79185253812998826142",
-        measurementId: "G-KXCS8374NK"
-    });
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+firebase.initializeApp({
+    apiKey: "AIzaSyBTMGjYkoraNTcr_MajS83QV6EjR3on8KY",
+    authDomain: "readytogo-e7bde.firebaseapp.com",
+    projectId: "readytogo-e7bde",
+    storageBucket: "readytogo-e7bde.firebasestorage.app",
+    messagingSenderId: "960914370023",
+    appId: "1:960914370023:web:0f79185253812998826142",
+    measurementId: "G-KXCS8374NK"
+});
 
-    const messaging = firebase.messaging();
+const messaging = firebase.messaging();
 
-    messaging.onBackgroundMessage((payload) => {
-        console.log("[firebase-messaging-sw.js] Received background message ", payload);
+messaging.onBackgroundMessage((payload) =>{
+    console.log("[firebase-messaging-sw.js] Received background message ", payload);
 
-        const notificationTitle = payload.notification.title;
-        const notificationOptions = {
-            body: payload.notification.body,
-            icon: payload.notification.image,
-        };
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+    body: payload.notification.body,
+    icon: payload.notification.image,
+    };
 
-        self.registration.showNotification(notificationTitle, notificationOptions);
-    });
-})();
+    self.registration.showNotification(notificationTitle, notificationOptions);
+    //ServiceWorkerRegistration
+})
